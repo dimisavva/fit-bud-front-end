839cd66a798261e6689c5febf87a584a35f7dafc
Mike Robles <mikej.robles@gmail.com>
2023-02-05 17:37:10 -0500

    creates MealCard

--- src/components/MealCard/MealCard.jsx
+++ src/components/MealCard/MealCard.jsx
@@ -1,3 +1,22 @@
 import { Link } from "react-router-dom"
 import styles from "./MealCard.module.css"
 
+import AuthorInfo from "../AuthorInfo/AuthorInfo"
+
+const MealCard = ({ meal }) => {
+  return (
+    <Link to={`/meals/${meal._id}`}>
+      <article className={styles.container}>
+        <header>
+          <span>
+            <h1>{meal.name}</h1>
+          </span>
+          <AuthorInfo content={meal} />
+        </header>
+        <p>{meal.description}</p>
+      </article>
+    </Link>
+  )
+}
+
+export default MealCard
