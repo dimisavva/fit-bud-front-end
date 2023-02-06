5442c60cac30abe07a2da841d43c6fbd344513c8
dimisavva <119276394+dimisavva@users.noreply.github.com>
2023-02-06 14:18:10 -0800

    create BlogDetails show

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+
 import styles from './BlogDetails.module.css'
+
+// components
+import Loading from "../Loading/Loading"
+import AuthorInfo from "../../components/AuthorInfo/AuthorInfo"
 
 // Services
 import * as blogService from '../../services/blogService'
@@ -20,11 +25,25 @@ const BlogDetails = (props) => {
   // Verify state with a console.log or React Dev Tools:
   console.log('Blog State:', blog)
 
-  return (
-    <main className={styles.container}>
-      Details
-    </main>
-  )
+if (!blog) return <Loading />
+
+return (
+  <main className={styles.container}>
+    <article>
+      <header>
+        <h3>{blog.category.toUpperCase()}</h3>
+        <h1>{blog.title}</h1>
+        <span>
+          <AuthorInfo content={blog} />
+        </span>
+      </header>
+      <p>{blog.text}</p>
+    </article>
+    <section>
+      <h1>Comments</h1>
+    </section>
+  </main>
+)
 }
 
 export default BlogDetails
