9be473e7107b15a59a77dc3c15ea366b0925c908
Manny <emaldonado1402@gmail.com>
2023-02-06 11:36:38 -0500

    Import Ex Card into Exercise List

--- src/components/ExerciseCard/ExerciseCard.jsx
+++ src/components/ExerciseCard/ExerciseCard.jsx
@@ -1,5 +1,25 @@
-// import styles from './ExerciseCard.module.css'
+import { Link } from "react-router-dom"
+import styles from './ExerciseCard.module.css'
 
-// const ExerciseCard = ({ createdAt }) => {
-//   const 
-// }
+import AuthorInfo from "../AuthorInfo/AuthorInfo"
+
+
+
+
+const ExerciseCard = ({ exercise}) => {
+  return (
+    <Link to={`/exercises/${exercise._id}`}>
+      <article className={styles.container}>
+        <header>
+          <span>
+            <h1>{exercise.title}</h1>
+          </span>
+          <AuthorInfo content={exercise.category} />
+        </header>
+        <p>{exercise.text}</p>
+      </article>
+    </Link>
+  )
+}
+
+export default ExerciseCard
