74284b688931a2712e7302d0663dc6736ad8cc88
Mike Robles <mikej.robles@gmail.com>
2023-02-09 12:49:51 -0500

    touches up profilecard

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,11 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from './ProfileCard.module.css'
 import ProfileIcon from '../../assets/icons/profile.png'
-import AuthorInfo from "../AuthorInfo/AuthorInfo"
-
-
-
-
 
 const ProfileCard = ({ profile }) => {
   const photo = profile?.photo ? profile.photo : ProfileIcon
@@ -14,17 +9,8 @@ const ProfileCard = ({ profile }) => {
       <article className={styles.container}>
         <img src={photo} alt='poopshit' />
         <header>
-
-            {/* <AuthorInfo content={profile}/> */}
           <span>
             <h1>{profile.name}</h1>
-
-          <span>
-            <>
-              <h1>{profile.name}</h1>
-              <h2><img src={profile.photo}/></h2>
-            </>
-
           </span>
         </header>
       </article>
