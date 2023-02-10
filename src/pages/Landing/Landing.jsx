072fdd01486b5f6a201ad3956959c674ddf3e2c1
dimisavva <119276394+dimisavva@users.noreply.github.com>
2023-02-10 07:55:45 -0800

    add mission statement

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -11,17 +11,17 @@ const Landing = ({user, profile}) => {
         <section className={styles.about}>
           <header>
             <h3></h3>
-            <h1>ABOUT US</h1>
+            <h1>Mission Statement</h1>
           </header>
           <article>
             <p>
-              There is an ongoing trend of diets and exercises that promises fast wieght loss. Those fad diets and exercises are made by 
+            This app was built by four strangers that became closer in the course of a week simply with a common goal in mind. FitBud is a culmination of our culture as a team, to connect people in achieving wellness of any level, together.
             </p>
           </article>
         </section>
       </main>
       <footer className={styles.footer}>
-        <p>© 2023 DJ MVMT. FitBudz RIGHTS RESERVED</p>
+        <p>© 2023 DJ MVMT - FitBuds Group. ALL RIGHTS RESERVED.</p>
       </footer>
     </>
   )
