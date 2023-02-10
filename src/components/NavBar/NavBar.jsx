061b9c83f80909c335b302c8d217ba3222fc778b
Mike Robles <mikej.robles@gmail.com>
2023-02-09 19:15:16 -0500

    icons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import { NavLink } from 'react-router-dom'
 import styles from './NavBar.module.css'
-// import Logo from '../../assets/branding/logo.svg'
 
 const NavBar = ({ user, handleLogout }) => {
 
